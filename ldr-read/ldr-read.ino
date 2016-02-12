34ac94729722f7d64d23ead03928b41aa38abb74
Arthur Carabott <arthurc@gmail.com>
2016-02-12 14:21:57 +0000

    refactor led into structs

--- ldr-read/ldr-read.ino
+++ ldr-read/ldr-read.ino
@@ -1,26 +1,41 @@
+struct LEDHardware {
+  int pin;
+  int state;
+};
+
+struct BlinkLED {
+  LEDHardware hardware;
+  unsigned long prevMillis;
+  unsigned long interval;
+};
+
+struct BlinkLED led;
+
 const int gLDRPin = 0;
 unsigned long gPrevLDRMillis = 0;
 
-const int gLEDPin = 13;
-int gLEDState = 0;
-unsigned long gPrevLEDMillis = 0;
+
 
 void setup() {
   Serial.begin(9600); // sets the serial port to 9600
-  pinMode(gLEDPin, OUTPUT);
-  pinMode(gLDRPin, INPUT);
+  led = {
+    .hardware = { .pin = 13, .state = 0 },
+    .prevMillis = 0,
+    .interval = 500,
+  };
+  pinMode(led.hardware.pin, OUTPUT);
+  // pinMode(gLDRPin, INPUT);
 }
 
-void toggleLED() {
-  gLEDState = abs(1 - gLEDState);
-  digitalWrite(gLEDPin, gLEDState);
+void toggleLED(struct LEDHardware& led) {
+  led.state = abs(1 - led.state);
+  digitalWrite(led.pin, led.state);
 }
 
-void tryLED(unsigned long currentMillis) {
-  const unsigned long ledInterval = 500;
-  if(currentMillis - gPrevLEDMillis >= ledInterval) {
-    gPrevLEDMillis = currentMillis;
-    toggleLED();
+void tryBlinkLED(struct BlinkLED& led, unsigned long currentMillis) {
+  if(currentMillis - led.prevMillis >= led.interval) {
+    led.prevMillis = currentMillis;
+    toggleLED(led.hardware);
   }
 }
 
@@ -39,5 +54,5 @@ void loop() {
   const unsigned long currentMillis = millis();
 
   tryLDR(currentMillis);
-  tryLED(currentMillis);
+  tryBlinkLED(led, currentMillis);
 }
