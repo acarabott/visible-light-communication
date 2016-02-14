fa0d04eb2f93d141402136156de860508b5b3a6d
Arthur Carabott <arthurc@gmail.com>
2016-02-14 14:35:34 +0000

    flash comm? good:)

--- message/message.ino
+++ message/message.ino
@@ -6,10 +6,15 @@ LED outputLED(4);
 LDR ldr(0);
 
 uint32_t prevMicros = 0;
-const uint32_t duration = 500000;
+const uint32_t duration = 100000;
+
+const uint8_t steps = 2;
+uint32_t idx = 0;
+uint8_t emitterState = 0;
 
 void setup()
 {
+  // Serial.begin(9600);
 }
 
 void loop()
@@ -18,8 +23,23 @@ void loop()
 
   if(curMicros - prevMicros >= duration) {
     prevMicros = curMicros;
-    emitterLED.toggle();
-    ldr.update();
-    outputLED.set(ldr.getBinary());
+
+    if(idx % steps == 0) {
+      // Serial.println("EMIT ON IGNORE!");
+      emitterLED.set(1);
+      ldr.update();
+      outputLED.set(ldr.getBinary());
+    } else {
+      // Serial.print("EMIT state: ");
+      // Serial.println(emitterState);
+      emitterLED.set(emitterState);
+      emitterState = abs(1 - emitterState);
+
+      // Serial.print("LDR READING: ");
+      // Serial.println(ldr.getBinary());
+
+    }
+    // Serial.println("---");
+    idx++;
   }
 }
