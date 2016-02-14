c41aee82745a05a315991206bd57b6b230d74939
Arthur Carabott <arthurc@gmail.com>
2016-02-14 14:46:17 +0000

    pattern nice and slow

--- message/message.ino
+++ message/message.ino
@@ -6,15 +6,16 @@ LED outputLED(4);
 LDR ldr(0);
 
 uint32_t prevMicros = 0;
-const uint32_t duration = 100000;
+const uint32_t duration = 500;
 
 const uint8_t steps = 2;
 uint32_t idx = 0;
 uint8_t emitterState = 0;
 
-#define PATTERN_LENGTH 2
-uint8_t pattern[PATTERN_LENGTH] = { 1, 0 };
+#define PATTERN_LENGTH 4
+uint8_t pattern[PATTERN_LENGTH] = { 1, 1, 1, 0 };
 uint32_t patternIdx = 0;
+uint32_t patternCount = 0;
 
 void setup()
 {
@@ -34,8 +35,11 @@ void loop()
     } else {
       emitterLED.set(emitterState);
       emitterState = pattern[patternIdx % PATTERN_LENGTH];
-      patternIdx++;
-      // emitterState = abs(1 - emitterState);
+
+      if(patternCount % 512 == 0) {
+        patternIdx++;
+      }
+      patternCount++;
     }
     idx++;
   }
