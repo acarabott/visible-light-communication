7f7bb132fa596d43f2a1a457b95107503c47513d
Arthur Carabott <arthurc@gmail.com>
2016-02-15 23:40:22 +0000

    hello world! :)

--- message/message.ino
+++ message/message.ino
@@ -3,17 +3,22 @@
 
 #define LOG
 
+#define COUNT_OF(x) ((sizeof(x)/sizeof(0[x])) / ((size_t)(!(sizeof(x) % sizeof(0[x])))))
+
+
 LED emitterLED(13);
 LED outputLED(4);
 LDR ldr(0);
 
 uint32_t prevMicros = 0;
-const uint32_t duration = 2000;
+const uint32_t duration = 1000;
 
 #define ENCODING_LENGTH 2
 #define BYTE 8
 #define PATTERN_LENGTH 8
-uint8_t pattern[PATTERN_LENGTH] = { 0,1,0,0,0,0,0,1 }; // ASCII A
+uint8_t text[] = "Hello World!\n";
+uint16_t textIdx = 0;
+uint8_t pattern[BYTE] = { 0,1,0,0,0,0,0,1 }; // ASCII A
 uint32_t patternIdx = 0;
 uint32_t patternCount = 0;
 uint32_t clockIdx = 0;
@@ -50,12 +55,24 @@ void printBuffer(uint8_t(&buffer)[BUFFER_SIZE]) {
   Serial.write(myByte);
 }
 
+void encodeChar(uint8_t c, uint8_t(&buffer)[BUFFER_SIZE]) {
+  for (uint16_t i = 0; i < BUFFER_SIZE; ++i) {
+    const uint8_t val = (c >> i) & 1;
+    buffer[(BUFFER_SIZE-1) - i] = val;
+  }
+}
+
 void loop()
 {
   uint32_t curMicros = micros();
 
   if(curMicros - prevMicros >= duration) {
     prevMicros = curMicros;
+
+    if(clockIdx % ENCODING_LENGTH == 0 && patternIdx == 0) {
+      encodeChar(text[textIdx], pattern);
+      textIdx = (textIdx + 1) % COUNT_OF(text);
+    }
 
     const uint8_t patternVal = pattern[patternIdx];
     // increase pattern index every 2 steps of the clock as each value is
