677645529130e5269eb1a2ff3f8092f9782555e1
Arthur Carabott <arthurc@gmail.com>
2016-02-15 16:46:46 +0000

    add buffer with printing

--- TODO.md
+++ TODO.md
@@ -1,5 +1,4 @@
 # VLC TODO
 
-- interpret signals
 - send start signal
 - interpret start of message
--- message/message.ino
+++ message/message.ino
@@ -11,13 +11,19 @@ uint32_t prevMicros = 0;
 const uint32_t duration = 2000;
 
 #define ENCODING_LENGTH 2
+#define BITS 8
+// send this at the start of the message for synchronisation
+const uint8_t startSignal[BITS] = { 1, 1, 1, 1, 1, 1, 1, 1 };
+
 #define PATTERN_LENGTH 8
 uint8_t pattern[PATTERN_LENGTH] = { 1, 0, 1, 1, 0, 1, 0, 0 };
 uint32_t patternIdx = 0;
 uint32_t patternCount = 0;
 uint32_t clockIdx = 0;
 
-uint8_t receiverValue = 0;
+#define BUFFER_SIZE BITS
+uint8_t receiverBuffer[BUFFER_SIZE];
+uint8_t receiverBufferIdx = 0;
 
 void setup()
 {
@@ -36,6 +42,14 @@ const uint8_t getManchesterEncoding(uint8_t signal, uint32_t clockIdx) {
                        (clockIdx % ENCODING_LENGTH == 0 ? 1 : 0);
 }
 
+// note max size is uint16_t
+void printBuffer(uint8_t(&buffer)[BUFFER_SIZE]) {
+  for(uint16_t i=0; i < BUFFER_SIZE; i++) {
+    Serial.print(buffer[i]);
+  }
+  Serial.print("\n");
+}
+
 void loop()
 {
   uint32_t curMicros = micros();
@@ -52,27 +66,25 @@ void loop()
 
     const uint8_t output = getManchesterEncoding(patternVal, clockIdx);
 
-    #ifdef LOG
-      // Serial.print(clockIdx % 8);
-      // Serial.print(": ");
-      // Serial.print(patternVal);
-      // Serial.print(" - ");
-      // Serial.println(output);
-    #endif
-
     emitterLED.set(output);
     ldr.update();
     // only update output every other frame, we
-    if(clockIdx % ENCODING_LENGTH == 1) {
-      receiverValue = ldr.getBinary();
-      // Serial.print("clock: ");
-      // Serial.print(clockIdx % ENCODING_LENGTH);
-      // Serial.print(" - ");
-      // Serial.println("read!");
-      Serial.println(receiverValue);
+    if(clockIdx % ENCODING_LENGTH == 0) {
+      // read value into buffer
+      const uint8_t receiverValue = ldr.getBinary();
+      receiverBuffer[receiverBufferIdx] = receiverValue;
+
+      // message complete
+      if(receiverBufferIdx == (BUFFER_SIZE - 1)) {
+        printBuffer(receiverBuffer);
+      }
+
+      // advance buffer index
+      receiverBufferIdx = (receiverBufferIdx + 1) % BUFFER_SIZE;
+
+      // Serial.println(receiverValue);
       // outputLED.set(receiverValue);
     }
-
 
     clockIdx++;
   }
