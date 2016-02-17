16f75f613af6bbc048d60dc3017b08405dd99545
Arthur Carabott <arthurc@gmail.com>
2016-02-17 13:13:12 +0000

    add phototransistor demo

--- phototransistor/phototransistor.ino
+++ phototransistor/phototransistor.ino
@@ -1,10 +1,31 @@
+const uint8_t photoPin = 1;
+uint32_t prevPhotoMicros = 0;
+uint32_t photoDur = 10000;
+
+uint32_t prevLedMicros = 0;
+uint32_t ledDur = 500000;
+uint8_t ledState = 0;
+const uint8_t ledPin = 13;
+
 void setup() {
   Serial.begin(9600); // sets the serial port to 9600
+  pinMode(photoPin, INPUT);
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-  const int val = analogRead(0); // read analog input pin 0
-  Serial.print(val); // prints the value read
-  Serial.print("\n"); // prints a space between the numbers
-  delay(10); // wait 10ms for next reading
+  uint32_t curMicros = micros();
+
+  if(curMicros - prevPhotoMicros >= photoDur) {
+    prevPhotoMicros = curMicros;
+
+    const int val = analogRead(photoPin);
+    Serial.println(val);
+  }
+
+  if(curMicros - prevLedMicros >= ledDur) {
+    prevLedMicros = curMicros;
+    digitalWrite(ledPin, ledState);
+    ledState = abs(ledState - 1);
+  }
 }
